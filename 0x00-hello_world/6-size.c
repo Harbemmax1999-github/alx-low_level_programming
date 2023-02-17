0550a58017cc92869710364c987b0fdd92e20de2
Harbemmax1999-github <abiodunogunleye627@gmail.com>
2023-02-16 22:40:09 -0800

    My third line of C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * code by harbemmax
- * Return: 0
-*/
+ * main - Entry point 
+ * Return: always 0 (succes)
+ */
 int main(void)
 {
-	char a:
-	int b:
-	long int c:
-	long long int d:
-	float f:
 
 	printf("Size of char is %lu byte(s)\n", sizeof(char));
 	printf("Size of int is %lu byte(s)\n", sizeof(int));
