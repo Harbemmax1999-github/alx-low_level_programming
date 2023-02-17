3a98dd5e49c4b3908840ca8ba677b3275ad23a01
Harbemmax1999-github <abiodunogunleye627@gmail.com>
2023-02-16 22:52:20 -0800

    My third line of C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 
-	printf("Size of char is %lu byte(s)\n", sizeof(char));
-	printf("Size of int is %lu byte(s)\n", sizeof(int));
-	printf("Size of long int is %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int is %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float is %lu byte(s)\n", sizeof(float));
+	printf("Size of a char is %lu byte(s)\n", sizeof(char));
+	printf("Size of an int is %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int is %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float is %lu byte(s)\n", sizeof(float));
 	return (0);
 }
