e9660a7f645b20635da4eecb2fb5023934f2a1c6
Harbemmax1999-github <abiodunogunleye627@gmail.com>
2023-03-09 13:40:54 -0800

    difficult task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -41,3 +41,19 @@ int is_palindrome(char *s)
 		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
+/**
+ * check_pal - check the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if is palindrome, 0 if not 
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s * i) != *(s * len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return check_pal(s, i + 1, len -1));
+}
