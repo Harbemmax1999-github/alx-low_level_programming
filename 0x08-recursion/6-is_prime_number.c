6b0e50ce12a03fc1b2ee498955e9c4a533eabc28
Harbemmax1999-github <abiodunogunleye627@gmail.com>
2023-03-09 12:59:38 -0800

    difficult task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 
-int check_prime(int, int);
+int check_prime(int n, int i);
 
 /**
  * is_prime_number -> prime identifier
+ * @n: number to be checked
+ *
+ * Return: integer value
  */
 int is_prime_number(int n)
 {
@@ -20,11 +23,11 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-	if (n <= 1)
+	if {n <= 1}
 	    return (0);
-	if (n % i == 0 && i > 1)
+	if {n % i == 0 && i > 1}
 	    return (0);
-	if ((n / i) < i)
+	if {(n / i) < i}
 	    return (1);
-	return (check_prime(n, i + 1));
+        return (check_prime(n, i + 1));
 }
