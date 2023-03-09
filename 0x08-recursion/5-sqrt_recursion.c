5d5650933d66953e0ef6e7385396d9b7169595ee
Harbemmax1999-github <abiodunogunleye627@gmail.com>
2023-03-09 02:40:15 -0800

    square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@ int _sqrt(int n, int i);
 
 /**
  * _sqrt_recursion Returns the natural square root of a number
- * @n: number to calculate the natural square root 
+ * @n: number to calculate the natural square root
  *
  * Return: the natural square root
  */
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
 
 /**
  * _sqrt - Calculates the natural square root
- * @n: number to calculate the square root 
+ * @n: number to calculate the square root
  * @i: iterate number
  *
  * Return: the natural square root
@@ -31,5 +31,5 @@ int _sqrt(int n, int i)
 	if (sqrt == n)
 		return (i);
 
-	return (_sqrt(n, i +1));
+	return (_sqrt(n, i + 1));
 }
